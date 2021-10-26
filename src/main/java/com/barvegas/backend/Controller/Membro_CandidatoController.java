b3954d147c74babd036847f92f37c4acc394d414
Bruno Lopes Correia de Souza <bruno.lc@outlook.com.br>
2021-10-26 12:37:55 -0300

    Commit Controller MembroCandidato

--- src/main/java/com/barvegas/backend/Controller/Membro_CandidatoController.java
+++ src/main/java/com/barvegas/backend/Controller/Membro_CandidatoController.java
@@ -1,14 +1,22 @@
 package com.barvegas.backend.Controller;
 
+import com.barvegas.backend.Model.Membro_CanditatoModel;
 import com.barvegas.backend.Service.Membro_CandidatoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api/membro")
 public class Membro_CandidatoController {
 
     @Autowired
     Membro_CandidatoService membroCandidatoService;
 
-    
+    @PostMapping
+    public Membro_CanditatoModel saveMembCandi(@RequestBody Membro_CanditatoModel newMemCan){
+        return membroCandidatoService.saveMembCandi(newMemCan);
+    }
 }
