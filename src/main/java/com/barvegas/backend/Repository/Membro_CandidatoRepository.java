a6902619f57cc4a9495c85ccfb29f05e2705c9c0
Bruno Lopes Correia de Souza <bruno.lc@outlook.com.br>
2021-10-26 12:16:13 -0300

    Commit Membro_CandidatoRepository

---
+++ src/main/java/com/barvegas/backend/Repository/Membro_CandidatoRepository.java
@@ -0,0 +1,7 @@
+package com.barvegas.backend.Repository;
+
+import com.barvegas.backend.Model.Membro_CanditatoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Membro_CandidatoRepository extends JpaRepository<Membro_CanditatoModel,Long> {
+}
