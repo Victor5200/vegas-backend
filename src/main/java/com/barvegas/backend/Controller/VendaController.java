b6ff0e1196c0188b00492ad1b786c8f5dc663fc9
Bruno Lopes Correia de Souza <bruno.lc@outlook.com.br>
2021-11-04 14:28:31 -0300

    .

--- src/main/java/com/barvegas/backend/Controller/VendaController.java
+++ src/main/java/com/barvegas/backend/Controller/VendaController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -30,7 +31,9 @@ public class VendaController {
 
     @PutMapping("/{idVenda}")
     @ApiOperation(value = "Adicona 1 item a uma venda")
-    public ResponseEntity<ModVenda> addItens (@PathVariable Long idVenda, Long idItem, Long qtd)
+    public ResponseEntity<ModVenda> addItens (@PathVariable(value = "idVenda")Long idVenda,
+                                              Long idItem,
+                                              Long qtd)
             throws Exception{
             return ResponseEntity.ok(serVenda.addItens(idVenda,idItem,qtd));
     }
