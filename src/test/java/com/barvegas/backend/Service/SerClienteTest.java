86845e6a3be50ec21610f80be5e14f713117e822
Bruno Lopes Correia de Souza <bruno.lc@outlook.com.br>
2021-11-12 15:53:28 -0300

    Iniciando trabalho de teste

---
+++ src/test/java/com/barvegas/backend/Service/SerClienteTest.java
@@ -0,0 +1,24 @@
+package com.barvegas.backend.Service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerClienteTest {
+
+    @Test
+    void getAllClientes() {
+    }
+
+    @Test
+    void getClienteById() {
+    }
+
+    @Test
+    void updateClienteById() {
+    }
+
+    @Test
+    void saveMembCandi() {
+    }
+}
