aa69e0c303a45ba51baa38b4ed80225577bf06ed
Victor Alex <victoralexsander5200@gmail.com>
2022-01-18 15:47:30 -0300

    ajustando autenticacao e versao do pom

--- src/main/java/com/barvegas/backend/Controller/AuthController.java
+++ src/main/java/com/barvegas/backend/Controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.barvegas.backend.Controller;
 
 import com.barvegas.backend.Model.AuthRequest;
+import com.barvegas.backend.Model.TokenDTO;
 import com.barvegas.backend.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +9,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Base64;
 
 @RestController
 public class AuthController {
@@ -20,16 +23,16 @@ public class AuthController {
 
 
     @PostMapping("/auth")
-    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+    public TokenDTO generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                             authRequest.getUserName(),
-                            authRequest.getPassword()));
+                            Base64.getEncoder().encodeToString(authRequest.getPassword().getBytes())));
         } catch (Exception ex) {
             throw new Exception("inavalid username/password");
         }
 
-        return jwtUtil.generateToken(authRequest.getUserName());
+        return TokenDTO.builder().token(jwtUtil.generateToken(authRequest.getUserName())).build();
     }
 
 }
---
+++ src/main/java/com/barvegas/backend/Model/TokenDTO.java
@@ -0,0 +1,14 @@
+package com.barvegas.backend.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TokenDTO {
+    private String token;
+}
